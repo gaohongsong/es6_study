7b6df20eb6431d85c64961b0eeae8d2109a7d67b
hongsonggao <hongsonggao@tencent.com>
2017-06-12 14:16:33 +0800

    function test

--- es6-feature/function.js
+++ es6-feature/function.js
@@ -170,3 +170,142 @@
     arr1.push(...arr3)
     console.log(arr1)
 }
+
+// 箭头函数
+{
+    // var fn = function(v){return v;}
+    var fn = v => v;
+
+    // var sum = function (num1, num2) { return num1 + num2; }
+    // var sum = (num1, num2) => num1 + num2;
+    var sum = (num1, num2) => {
+        // 多行
+        var tmp = 0;
+        tmp = tmp + num1 + num2;
+        return tmp;
+    }
+    console.log(sum(1, 2));
+
+    // var getTempItem = id => ({ id: id, name: "Temp" });
+    var getTempItem = id => {
+        return { id: id, name: "Temp" };
+    };
+    console.log(getTempItem(1));
+
+    // 变量解析
+    const full = ({ first, last }) => first + ' ' + last;
+    console.log(full({ first: 'Gao', last: 'Song' }));
+
+    const numbers = (...nums) => nums;
+    numbers(1, 2, 3, 4, 5)
+    // [1,2,3,4,5]
+
+    const headAndTail = (head, ...tail) => [head, tail];
+    headAndTail(1, 2, 3, 4, 5);
+    // [1,[2,3,4,5]]
+
+    // 简化回调
+    // [1, 2, 3].map(function (x) { return x * x })
+    [1, 2, 3].map(x => x * x);
+
+    // 函数内的this为定义时所在的对象
+    // 箭头函数根本没有自己的this，导致内部的this就是外层代码块的this
+    // 同理：arguments、super、new.target
+    function Timer() {
+        this.s1 = 0;
+        this.s2 = 0;
+        // 箭头函数
+        setInterval(() => {
+            // this指向this绑定定义时所在的作用域（即Timer函数）
+            // console.log("=>", this);
+            this.s1++;
+        }, 1000);
+        // 普通函数
+        setInterval(function () {
+            // this指向运行时所在的作用域（即全局对象）
+            this.s2++;
+            // console.log(this);
+        }, 1000);
+    }
+
+    var timer = new Timer();
+
+    setTimeout(() => console.log('s1: ', timer.s1), 3100);
+    setTimeout(() => console.log('s2: ', timer.s2), 3100);
+    // s1: 3
+    // s2: 0    
+
+    // 箭头函数转成 ES5 的代码如下
+    // ES6
+    function foo() {
+        setTimeout(() => {
+            console.log('id:', this.id);
+        }, 100);
+    }
+    // ES5
+    function foo() {
+        var _this = this;
+
+        setTimeout(function () {
+            console.log('id:', _this.id);
+        }, 100);
+    }
+
+    // function foo() {
+    //     return () => {
+    //         return () => {
+    //             return () => {
+    //                 console.log('id:', this.id);
+    //             };
+    //         };
+    //     };
+    // }
+
+    // var f = foo.call({ id: 1 });
+
+    // var t1 = f.call({ id: 2 })()(); // id: 1
+    // var t2 = f().call({ id: 3 })(); // id: 1
+    // var t3 = f()().call({ id: 4 }); // id: 1    
+    function foo() {
+        setTimeout(() => {
+            console.log('args:', arguments);
+        }, 100);
+    }
+
+    foo(2, 4, 6, 8)
+    // args: [2, 4, 6, 8]
+
+    // 嵌套的箭头函数
+    function insert(value) {
+        return {
+            into: function (array) {
+                return {
+                    after: function (afterValue) {
+                        array.splice(array.indexOf(afterValue) + 1, 0, value);
+                        return array;
+                    }
+                };
+            }
+        };
+    }
+
+    console.log(insert(2).into([1111, 3222]).after(1));
+    //[1111, 2, 3222]
+
+    let insert1 = (value) => ({
+        into: (array) => ({
+            after: (afterValue) => {
+                array.splice(array.indexOf(afterValue) + 1, 0, value);
+                return array;
+            }
+        })
+    })
+    console.log(insert1(2).into([1111, 3222]).after(1));
+    
+    
+    const plus1 = a => a + 1;
+    const mult2 = a => a * 2;
+
+    mult2(plus1(5))
+    // 12
+}
